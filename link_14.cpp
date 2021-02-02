d0a5e73fec35dbfa16c40e429aa695df433a09d5
1059Mustafa <72117500+1059Mustafa@users.noreply.github.com>
2021-02-02 16:29:08 +0530

    Add files via upload

---
+++ link_14.cpp
@@ -0,0 +1,32 @@
+Node* findIntersection(Node* head1, Node* head2)
+{
+    Node* start=NULL;
+    Node* end=NULL;
+    while(head1&&head2)
+    {
+        if(head1->data==head2->data)
+        {
+            if(start==NULL)
+            {
+                start=new Node(head1->data);
+                end=start;
+            }
+            else
+            {
+                end->next=new Node(head1->data);
+                end=end->next;
+            }
+            head1=head1->next;
+            head2=head2->next;
+        }
+        else if(head1->data>head2->data)
+        {
+            head2=head2->next;
+        }
+        else
+        {
+            head1=head1->next;
+        }
+    }
+    return start;
+}
